79f5fb4dedde49b0088f185d50a5ed86d7491d41
ashcroft147 <pd050786@VDIPS.POSCO.DOM>
2017-08-14 14:05:59 +0900

    Add Action Creator for signin

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+
+
+export function signinUser({email, password}) {
+    // Submit email/password to the server
+
+    // If good
+    // - Update state to indicate user is authenticated
+    // - Save the JWT Token
+    // - redirect to the route '/feature'
+
+    // If bas
+    // - Show an error to the user
+}
--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -13,15 +13,15 @@ class Signin extends Component {
             <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
                 <fieldset className="form-group">
                     <label>Email:</label>
-                    <input className="form-control"/>
+                    <input {...email} className="form-control"/>
                 </fieldset>
                 <fieldset className="form-group">
                     <label>Password:</label>
-                    <input className="form-control"/>
+                    <input {...password} className="form-control"/>
                 </fieldset>        
                 <button action="submit" className="btn btn-primary">Sign in</button>
             </form>
-        )
+        );
     }
 }
 
